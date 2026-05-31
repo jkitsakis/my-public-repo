23a110cc596fcdaee1082f4952d08f9367041479
yannis <yannis@users.noreply.github.com>
2026-05-31 09:09:11 +0300

    restart network manager

--- kubuntu_configurations/3-wifi/restart_networkmanager.sh
+++ kubuntu_configurations/3-wifi/restart_networkmanager.sh
@@ -3,10 +3,15 @@
 sleep 5
 # Check NetworkManager status
 if systemctl is-active --quiet NetworkManager; then
-	echo "NetworkManager is running."
+	echo "NetworkManager is running.Restarting ..."
+	sudo systemctl stop NetworkManager
+	sudo systemctl start NetworkManager
+	sudo systemctl enable NetworkManager
+	echo "NetworkManagerRestarted!!!"
 else
 	echo "NetworkManager is NOT running. Starting NetworkManager..."
 	sudo systemctl start NetworkManager
 	# Optional: enable NetworkManager to start on boot
 	sudo systemctl enable NetworkManager
+	echo "NetworkManagerRestarted!!!"
 fi
